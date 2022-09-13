bf1484cde53f1f83230edc4bbb13786e3627f5be
fanzhuo <fans068@163.com>
2022-09-13 14:04:22 +0800

    1.开篇 2.服务拆分

---
+++ apps/order/admin/internal/svc/servicecontext.go
@@ -0,0 +1,15 @@
+package svc
+
+import (
+	"github.com/fans1992/lebron/apps/order/admin/internal/config"
+)
+
+type ServiceContext struct {
+	Config config.Config
+}
+
+func NewServiceContext(c config.Config) *ServiceContext {
+	return &ServiceContext{
+		Config: c,
+	}
+}
