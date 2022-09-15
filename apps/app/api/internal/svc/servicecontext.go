8ded47eb0d89f95ead8b74e865cf4cfea8c68ae8
fanzhuo <fans068@163.com>
2022-09-15 16:14:04 +0800

    三、API定义和表结构设计

--- apps/app/api/internal/svc/servicecontext.go
+++ apps/app/api/internal/svc/servicecontext.go
@@ -2,14 +2,21 @@ package svc
 
 import (
 	"github.com/fans1992/lebron/apps/app/api/internal/config"
+	"github.com/fans1992/lebron/apps/order/rpc/order"
+	"github.com/fans1992/lebron/apps/product/rpc/product"
+	"github.com/zeromicro/go-zero/zrpc"
 )
 
 type ServiceContext struct {
-	Config config.Config
+	Config     config.Config
+	OrderRPC   order.Order
+	ProductRPC product.Product
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
 	return &ServiceContext{
-		Config: c,
+		Config:     c,
+		OrderRPC:   order.NewOrder(zrpc.MustNewClient(c.OrderRPC)),
+		ProductRPC: product.NewProduct(zrpc.MustNewClient(c.ProductRPC)),
 	}
 }
